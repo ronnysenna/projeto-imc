bab36bbf9f4a458c1f80004461551005d78f4a55
RonnySenna <ronnysenna@yahoo.com>
2024-09-05 18:27:20 -0300

    Add código C++

---
+++ imc.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    float imc = 0, peso = 0, altura = 0;
+
+    do {
+        printf("\n \t Digite seu peso (em kg): ");
+        scanf("%f", &peso);
+        if (peso < 30 || peso > 200) {
+            printf("\n \t Peso inválido. Por favor, digite um valor entre 30 kg e 200 kg.\n");
+        }
+    } 
+	while (peso < 30 || peso > 200);
+
+    do {
+        printf("\n \t Digite sua altura (em metros): ");
+        scanf("%f", &altura);
+        if (altura < 1.2 || altura > 2.5) {
+            printf("\n \t Altura inválida. Por favor, digite um valor entre 1,2 m e 2,5 m.\n");
+        }
+    } 
+	while (altura < 1.2 || altura > 2.5);
+
+    imc = peso / (altura * altura);
+
+    printf("\n \t Seu IMC e = %.2f\n", imc);
+    
+    if (imc < 18.5) {
+        printf ("\n\t Voce esta abaixo do peso normal");
+    }
+    else if (imc >= 18.5 && imc <= 24.9) {
+        printf ("\n\t Seu peso esta normal");
+    }
+    else if (imc >= 25.0 && imc <= 29.9) {
+        printf ("\n\t Voce esta com excesso de peso");
+    }
+    else if (imc >= 30.0 && imc <= 34.9) {
+        printf ("\n\t Voce este em obesidade classe I");
+    }
+    else if (imc >= 35.0 && imc <= 39.9) {
+        printf ("\n\t Voce este em obesidade classe II");
+    }
+    else {
+        printf ("\n\t Você está em obesidade classe III");
+    }
+
+    return 0;
+}
+
